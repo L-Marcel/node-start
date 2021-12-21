da5155fb95e4da71270687e43cb21d97fc2c0d40
LMarcel <lmgh1312@gmail.com>
2021-12-21 16:37:36 -0300

    Adicionando método para retornar um error

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
+//import { getError } from "./utils/errors";
 
 const routes = express.Router();
 
 routes.get("/", (req: Req, res: Res) => {
+  //return getError(res, 400, "Error example");
   return res.json({
     message: "Hello world!!!",
     create_at: new Date()
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,2 +1,3 @@
 declare type Req = import("express").Request;
 declare type Res = import("express").Response;
+declare type Err = { message: string; };
