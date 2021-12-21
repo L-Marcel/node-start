09101b0d59dd1cd06f4c76e35e212671ccd6c604
LMarcel <lmgh1312@gmail.com>
2021-12-21 20:07:28 -0300

    Adicionando novas tipagens

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,16 @@
+declare module Express {
+  interface Request {
+    //...
+  };
+  interface Response {
+    //...
+  };
+  interface NextFunction {
+    //...
+  };
+};
+
 declare type Req = import("express").Request;
 declare type Res = import("express").Response;
+declare type Next = import("express").NextFunction;
 declare type Err = { message: string; };
