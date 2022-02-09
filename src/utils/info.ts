c35c1f2608a7c8569762158958ee1cffc015c5f9
LMarcel <lmgh1312@gmail.com>
2022-02-09 13:56:21 -0300

    Melhorando script

--- src/utils/info.ts
+++ src/utils/info.ts
@@ -1,6 +1,6 @@
 function getInfo(message: string) {
   if(process.env.NODE_ENV?.trim() === "dev") {
-    console.log(`[\x1b[34mINFO\x1b[0m] \x1b[30m\x1b[1m${new Date().toLocaleTimeString()}\x1b[0m ${message}`);
+    console.log(`[\x1b[36mINFO\x1b[0m] \x1b[30m\x1b[1m${new Date().toLocaleTimeString()}\x1b[0m ${message}`);
   };
 };
 
